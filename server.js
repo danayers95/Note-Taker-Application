9b558ee3496fe1cd08751aef31cfbd686761a6b5
Donkey <danglayers@gmail.com>
2020-07-28 20:59:32 -0400

    set up express/set up port

---
+++ server.js
@@ -0,0 +1,11 @@
+// set up app with express
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+// Make Express listen on port 3000
+const app = express();
+const PORT = 3000;
+
+
