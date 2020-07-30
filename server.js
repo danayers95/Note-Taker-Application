bcff38433e6791bf2c39d44abb948da986ade37c
Donkey <danglayers@gmail.com>
2020-07-30 13:19:37 -0400

    added routes

--- server.js
+++ server.js
@@ -8,4 +8,23 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+let notes = require("./db/db.json");
 
+// server begins listening
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
+// create routes for html
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
