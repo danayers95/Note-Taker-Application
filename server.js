402f1536e89f428be22248916612d43871d62635
Donkey <danglayers@gmail.com>
2020-07-30 20:58:46 -0400

    fixed issue with displaying saved notes

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+// routes db.json to the html page
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -39,46 +49,79 @@ app.get("/api/notes", function (req, res) {
     });
 });
 
-// new note is created
-app.post("/api/notes", function (req, res) {
-    let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    let id = randLetter + Date.now();
-    let newNote = {
-        id: id,
-        title: req.body.title,
-        text: req.body.text,
-    };
-    console.log(typeof notes);
-    notes.push(newNote);
-    const stringifyNote = JSON.stringify(notes);
-    res.json(notes);
-    fs.writeFile("db/db.json", stringifyNote, (err) => {
-        if (err) console.log(err);
-        else {
-            console.log("Note successfully saved");
-        }
-    });
-});
+// new note is created 
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
 
-// if user would like to delete note:
-/* app.delete("/api/notes/:id", function (req, res) {
-    let noteID= req.params.id;
-    fs.readFile("db/db.json", "utf8", function (err, data) {
-        let updatedNotes = JSON.parse(data).filter((note) => {
-            console.log("note.id", note.id);
-            console.log("noteID", noteID);
-            return note.id !== noteID;
-        });
-        notes = updatedNotes;
-        const stringifyNote = JSON.stringify(updatedNotes);
-        fs.writeFile("db/db.json", stringifyNote, (err) => {
-            if (err) console.log(err);
-            else {
-                console.log("Note deleted successfully");
-            }
-        });
-        res.json(stringifyNote);
-    });
-}); */
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved successfully to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+
+
+// new note is created
+// app.post("/api/notes", function (req, res) {
+//     let randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+//     let id = randLetter + Date.now();
+//     let newNote = {
+//         id: id,
+//         title: req.body.title,
+//         text: req.body.text,
+//     };
+//     console.log(typeof notes);
+//     notes.push(newNote);
+//     const stringifyNote = JSON.stringify(notes);
+//     res.json(notes);
+//     fs.writeFile("db/db.json", stringifyNote, (err) => {
+//         if (err) console.log(err);
+//         else {
+//             console.log("Note successfully saved");
+//         }
+//     });
+// });
+
+// // if user would like to delete note:
+// app.delete("/api/notes/:id", function (req, res) {
+//     let noteID= req.params.id;
+//     fs.readFile("db/db.json", "utf8", function (err, data) {
+//         let updatedNotes = JSON.parse(data).filter((note) => {
+//             console.log("note.id", note.id);
+//             console.log("noteID", noteID);
+//             return note.id !== noteID;
+//         });
+//         notes = updatedNotes;
+//         const stringifyNote = JSON.stringify(updatedNotes);
+//         fs.writeFile("db/db.json", stringifyNote, (err) => {
+//             if (err) console.log(err);
+//             else {
+//                 console.log("Note deleted successfully");
+//             }
+//         });
+//         res.json(stringifyNote);
+//     });
+// });
 
 
